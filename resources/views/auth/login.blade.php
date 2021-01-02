cfff0c24d6a33b59941b98ba2b9aa5b8ccd21dcb
Yassin Maher <professor.yassin@yahoo.com>
2021-01-03 01:19:27 +0200

    Password reset #32

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,10 +12,10 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="mobile" class="col-md-4 col-form-label text-md-right">Mobile</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">Email</label>
 
                             <div class="col-md-6">
-                                <input id="mobile" type="text" class="form-control @error('mobile') is-invalid @enderror" name="mobile" value="{{ old('mobile') }}" required autofocus>
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @error('mobile')
                                     <span class="invalid-feedback" role="alert">
