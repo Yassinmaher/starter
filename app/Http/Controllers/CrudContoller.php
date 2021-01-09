9327991b9990e14f490f4adc5f3e81d1e744ef02
Yassin Maher <professor.yassin@yahoo.com>
2021-01-10 00:28:08 +0200

    laravel validate request before inserting into database #48

--- app/Http/Controllers/CrudContoller.php
+++ app/Http/Controllers/CrudContoller.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Offer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CrudContoller extends Controller
 {
@@ -13,11 +14,15 @@ class CrudContoller extends Controller
     }
 
     public function store(Request $request) {
-//        Offer::create([
-//            'name' => 'Offer4',
-//            'price' => '3000',
-//            'details' => 'Offer details'
-//        ]);
+
+        $rules = $this -> getRules();
+        $messages =$this -> getMessages();
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return $validator->errors();
+        }
+
         Offer::create([
             'name' => $request->name,
             'price'=>$request->price,
@@ -28,6 +33,25 @@ class CrudContoller extends Controller
 
     public function create() {
         return view('offers.create');
+    }
+
+    protected function getRules() {
+        return [
+            'name' => 'required|max:100|unique:offers,name',
+            'price' => 'required|numeric',
+            'details' => 'required'
+        ];
+    }
+
+    protected function getMessages() {
+        return [
+            'name.required' => 'Sorry Offer Name Is Required',
+            'name.max' => 'Sorry The Maximum Length Is 100',
+            'name.unique' => 'Sorry This Name Is Taken Already',
+            'price.required' => 'Sorry Price Is Required',
+            'price.numeric' => 'Sorry The Only Allowed Data Type Is Number',
+            'details.required' => 'Sorry Details Is Required',
+        ];
     }
 
 }
